eec75f3baee31e469f554574e0acf338e44c2671
Jim Anders <jimanders223@gmail.com>
2019-08-14 10:53:05 -0400

    Put git hooks in proper directory

--- default.nix
+++ default.nix
@@ -18,11 +18,11 @@ in {
     etc = {
       "per-user/alacritty/alacritty.yml".text = import ./config/alacritty.nix { zsh = pkgs.zsh; };
       "per-user/.gitconfig".text = import ./config/gitconfig.nix { home = home; };
-      "per-user/.git_template/ctags".text = import ./config/git_template/ctags.nix { ctags = pkgs.ctags; };
-      "per-user/.git_template/post-checkout".text = import ./config/git_template/post-checkout.nix;
-      "per-user/.git_template/post-commit".text = import ./config/git_template/post-commit.nix;
-      "per-user/.git_template/post-merge".text = import ./config/git_template/post-merge.nix;
-      "per-user/.git_template/post-rewrite".text = import ./config/git_template/post-rewrite.nix;
+      "per-user/.git_template/hooks/ctags".text = import ./config/git_template/ctags.nix { ctags = pkgs.ctags; };
+      "per-user/.git_template/hooks/post-checkout".text = import ./config/git_template/post-checkout.nix;
+      "per-user/.git_template/hooks/post-commit".text = import ./config/git_template/post-commit.nix;
+      "per-user/.git_template/hooks/post-merge".text = import ./config/git_template/post-merge.nix;
+      "per-user/.git_template/hooks/post-rewrite".text = import ./config/git_template/post-rewrite.nix;
       "per-user/.gitignore_global".text = import ./config/gitignore.nix;
     };
     shells = [ pkgs.zsh ];
